21ae5cae4c374622641d743f316de24d83d2d303
snailvoyager <kyj88514@gmail.com>
2020-10-02 16:06:11 +0900

    Initial commit

---
+++ eatgo-api/src/main/java/com/snailvoyager/springbooteatgo/SpringbootEatgoApplication.java
@@ -0,0 +1,13 @@
+package com.snailvoyager.springbooteatgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEatgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEatgoApplication.class, args);
+	}
+
+}
---
+++ eatgo-api/src/test/java/com/snailvoyager/springbooteatgo/SpringbootEatgoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snailvoyager.springbooteatgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEatgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
