11bde564da6271dffe2b5e3a15185544879e588f
snailvoyager <kyj88514@gmail.com>
2020-10-06 21:07:21 +0900

    spring IOC DI

---
+++ eatgo-api/src/main/java/com/snailvoyager/springbooteatgo/domain/RestaurantRepositoryImpl.java
@@ -0,0 +1,30 @@
+package com.snailvoyager.springbooteatgo.domain;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class RestaurantRepositoryImpl implements RestaurantRepository {
+
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+    public RestaurantRepositoryImpl() {
+        restaurants.add(new Restaurant(1004L, "Bab zip", "Seoul"));
+        restaurants.add(new Restaurant(2020L, "Cyber Food", "Seroul"));
+    }
+
+    @Override
+    public List<Restaurant> findAll(){
+        return restaurants;
+    }
+
+    @Override
+    public Restaurant findById(Long id) {
+        return restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElse(null);
+    }
+}
